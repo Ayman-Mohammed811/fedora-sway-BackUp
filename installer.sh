b97d94df5ed0513cc327ff3ccd4d0bb15e86417f
Ayman Mohammed <ayman.m.mady@gmail.com>
2025-11-01 15:01:19 +0200

    make installer script more usable

--- installer.sh
+++ installer.sh
@@ -1,10 +1,24 @@
 
-sudo dnf install kitty  fastfetch git gh g++ gcc gdb fzf zsh wf-recorder kdeconnectd copyq
+sudo dnf install alacritty fastfetch git gh g++ gcc gdb fzf zsh wf-recorder kdeconnectd copyq
 
 sudo dnf install flatpak ninja-build cmake mako lsd qt5ct qt6ct  curl htop
 
 sudo dnf remove firefox 
 
-sudo firewall-cmd --permanent --add-service=kdeconnect\nsudo firewall-cmd --reload
+sudo firewall-cmd --permanent --add-service=kdeconnect
 
-curl -fsS https://dl.brave.com/install.sh | CHANNEL=nightly sh
+sudo firewall-cmd --reload
+
+curl -fsS https://dl.brave.com/install.sh | sh
+
+cp -r .config/* ~/.config
+
+mkdir ~/themes
+cp -r themes/*  ~/themes
+
+cp -r ZSH/* ~/
+
+chsh -s $(which zsh)
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
